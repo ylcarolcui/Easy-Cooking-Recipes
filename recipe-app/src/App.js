7958b46886e325af3ad0cc80ffa4cfa4ce2266af
ylcarolcui <yiling_1212@hotmail.com>
2020-04-30 18:38:01 -0400

    added css for footer

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -24,7 +24,7 @@ function App() {
 	}, []);
 
 	function getRecipes(searchString) {
-		const url = `https://api.edamam.com/search?q=${searchString}&app_id=${searchOptions.APP_ID}&app_key=${searchOptions.key}&from=0&to=12&calories=500-1000`;
+		const url = `https://api.edamam.com/search?q=${searchString}&app_id=${searchOptions.APP_ID}&app_key=${searchOptions.key}&from=0&to=20&calories=500-1000`;
 		console.log(url);
 
 		fetch(url)
--- recipe-app/src/Components/Footer.js
+++ recipe-app/src/Components/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
-
+import './Footer.css'
 function Footer () {
   return (
-    <>
-    <footer>
-      <p>© Carol Cui. All rights reserved.</p>
-    </footer>
-    </>
-  )
+			<footer>
+					<p> &copy; Carol Cui- 2020 &hearts;</p>
+			</footer>
+	);
 }
 export default Footer
